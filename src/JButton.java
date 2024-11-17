2f54ff93ea7255c176d0ebe1b7199fa710da055a
Naila Khairyah <naylakhairyahkhairyah@gmail.com>
2024-11-17 14:24:37 +0700

    commit pertama

---
+++ src/JButton.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author acer
+ */
+public class JButton {
+
+    JButton(String hitung_Diskon) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
